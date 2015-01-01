9540034e6d86f0e8d32b9ba5003d344590fed3ba
Justin Love <git@JustinLove.name>
2015-01-01 14:07:52 -0600

    copy to server mod

---
+++ Gruntfile.js
@@ -0,0 +1,93 @@
+var prompt = require('prompt')
+prompt.start()
+
+var modPath = '../../server_mods/com.wondible.pa.server_mod_player_guide.server/'
+var stream = 'stable'
+var media = require('./lib/path').media(stream)
+console.log(media)
+
+module.exports = function(grunt) {
+  // Project configuration.
+  grunt.initConfig({
+    copy: {
+      mod: {
+        files: [
+          {
+            src: [
+              'modinfo.json',
+              'LICENSE.txt',
+              'README.md',
+              'CHANGELOG.md',
+              'com.wondible.*/**',
+              'ui/**',
+              'pa/**'],
+            dest: modPath,
+          },
+        ],
+      },
+      modinfo: {
+        files: [
+          {
+            src: ['modinfo.json'],
+            dest: modPath,
+          },
+        ],
+        options: {
+          process: function(content, srcpath) {
+            var info = JSON.parse(content)
+            info.date = require('dateformat')(new Date(), 'yyyy/mm/dd')
+            info.identifier = info.identifier.replace('client', 'server')
+            info.context = 'server'
+            delete(info.scenes)
+            delete(info.priority)
+            console.log(info.identifier, info.version, info.date)
+            return JSON.stringify(info, null, 2)
+          }
+        }
+      },
+      new_game: {
+        files: [
+          {
+            expand: true,
+            cwd: media,
+            src: [
+              'ui/main/game/new_game/new_game.js'
+            ],
+            dest: modPath,
+          },
+        ],
+        options: {
+          process: function(contents, path) {
+            return contents.replace('// inject per scene mods', 'loadMods(["coui://ui/mods/server_mod_player_guide/new_game.js"])')
+          }
+        }
+      },
+      guide: {
+        files: [
+          {
+            expand: true,
+            cwd: media,
+            src: [
+              'ui/main/game/guide/guide.js'
+            ],
+            dest: modPath,
+          },
+        ],
+        options: {
+          process: function(contents, path) {
+            return contents.replace('// inject per scene mods', 'loadMods(["coui://ui/mods/server_mod_player_guide/guide.js"])')
+          }
+        }
+      }
+    },
+    clean: ['ui/main', modPath],
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+
+  // Default task(s).
+  grunt.registerTask('default', ['copy']);
+
+};
+
