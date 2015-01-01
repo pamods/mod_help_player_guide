22a65f69474e239087cba3acb06e4d26a4284ae2
Justin Love <git@JustinLove.name>
2015-01-01 06:57:55 -0600

    rename

--- ui/mods/server_mod_player_guide/guide.js
+++ ui/mods/server_mod_player_guide/guide.js
@@ -4,14 +4,13 @@
   var addArticle = function(title, url) {
     $('.section_controls ul').append('<li><a class="btn_std_ix" href="'+url+'"target="navigation">'+title+'</a></li>')
   }
-  addArticle("Player Guide Ex", "coui://ui/mods/player_guide_ex/article/player_guide_ex.html")
 
   var registerMods = function(mods) {
     mods.forEach(function(mod) {
       var attrs = Object.keys(mod).map(function(key) {
         return [key, encodeURIComponent(mod[key])].join('=')
       })
-      addArticle(mod.display_name, "coui://ui/mods/player_guide_ex/article/mod.html?" + attrs.join('&'))
+      addArticle(mod.display_name, "coui://ui/mods/server_mod_player_guide/article/mod.html?" + attrs.join('&'))
     })
   }
 
